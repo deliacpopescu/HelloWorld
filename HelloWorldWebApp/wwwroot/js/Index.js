235b5356873e189f9b4fc04b4fdd3658f80cc9a9
delia_popescu <delia.popescu@principal33.com>
2021-08-03 10:38:01 +0300

    commit index.js

---
+++ HelloWorldWebApp/wwwroot/js/Index.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    // see https://api.jquery.com/click/
+    $("#createButton").click(function () {
+        var newcomerName = $("#nameField").val();
+        // Remember string interpolation
+        $("#list").append(`<li>${newcomerName}</li>`);
+        $("#nameField").val("");
+    })
+});
