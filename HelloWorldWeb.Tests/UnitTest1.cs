be5623d25f44302005dfd2f98fad745875eea5b5
delia_popescu <delia.popescu@principal33.com>
2021-08-09 01:03:04 +0300

    -improve AddTeamMember test -create AddTeamMemberWithId and UpdateTeamMemberNoId  tests

--- HelloWorldWeb.Tests/UnitTest1.cs
+++ HelloWorldWeb.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using HelloWorldWebApp.Models;
 using HelloWorldWebApp.Services;
+using System.Linq;
 using Xunit;
 
 namespace HelloWorldWeb.Tests
@@ -7,39 +8,44 @@ namespace HelloWorldWeb.Tests
     public class TeamServiceTest
     {
         [Fact]
-        public void AddTeamMemberToTheTeam()
+        public void AddTeamMember()
         {
             // Assume
 
             ITeamService teamService = new TeamService();
+            int id = teamService.GetTeamInfo().TeamMembers.Max(member => member.Id);
+            int size = teamService.GetTeamInfo().TeamMembers.Count();
 
             // Act
 
-            teamService.AddTeamMember(new Member("George", 5));
+            teamService.AddTeamMember(new Member("George"));
 
             // Assert
 
-            Assert.Equal(5, teamService.GetTeamInfo().TeamMembers.Count);
-
+            Assert.Equal(size + 1, teamService.GetTeamInfo().TeamMembers.Count);
+            Assert.Equal(id + 1, teamService.GetTeamInfo().TeamMembers[size].Id);
+            Assert.Equal("George", teamService.GetTeamInfo().TeamMembers[size].Name);
         }
 
         [Fact]
-        public void DeleteTeamMemberToTheTeam()
+        public void AddTeamMemberWithId()
         {
             // Assume
 
             ITeamService teamService = new TeamService();
-            int count = teamService.GetTeamInfo().TeamMembers.Count;
+            int id = teamService.GetTeamInfo().TeamMembers.Max(member => member.Id);
+            int size = teamService.GetTeamInfo().TeamMembers.Count();
 
             // Act
 
-            teamService.DeleteTeamMember(2);
+            teamService.AddTeamMember(new Member("Mike", 8));
 
             // Assert
 
-            Assert.False(teamService.GetTeamInfo().TeamMembers.Exists(r => r.Id == 2));
-            Assert.Equal(count - 1 , teamService.GetTeamInfo().TeamMembers.Count);
-
+            Assert.Equal(size + 1, teamService.GetTeamInfo().TeamMembers.Count);
+            Assert.NotEqual(8, teamService.GetTeamInfo().TeamMembers[size].Id);
+            Assert.Equal(id + 1, teamService.GetTeamInfo().TeamMembers[size].Id);
+            Assert.Equal("Mike", teamService.GetTeamInfo().TeamMembers[size].Name);
         }
 
         [Fact]
@@ -55,8 +61,45 @@ namespace HelloWorldWeb.Tests
 
             // Assert
 
-            Assert.Equal( "Anna", teamService.GetTeamInfo().TeamMembers[1].Name);
-           
+            Assert.Equal("Anna", teamService.GetTeamInfo().TeamMembers[1].Name);
+
+
+        }
+
+        [Fact]
+        public void UpdateTeamMemberNoId()
+        {
+            // Assume
+            ITeamService teamService = new TeamService();
+            TeamInfo teamInfo = teamService.GetTeamInfo();
+            Member member = new Member("Anna", 100);
+
+            // Act
+
+            teamService.UpdateTeamMember(member);
+
+            // Assert
+
+            Assert.Null(teamService.GetTeamInfo().TeamMembers.ElementAtOrDefault(99));
+            Assert.Equal(teamInfo, teamService.GetTeamInfo());
+        }
+
+        [Fact]
+        public void DeleteTeamMember()
+        {
+            // Assume
+
+            ITeamService teamService = new TeamService();
+            int count = teamService.GetTeamInfo().TeamMembers.Count;
+
+            // Act
+
+            teamService.DeleteTeamMember(2);
+
+            // Assert
+
+            Assert.False(teamService.GetTeamInfo().TeamMembers.Exists(r => r.Id == 2));
+            Assert.Equal(count - 1 , teamService.GetTeamInfo().TeamMembers.Count);
 
         }
     }
