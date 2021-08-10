99abb172332f654d31c2fc266ceda0ade603bcfe
delia_popescu <delia.popescu@principal33.com>
2021-08-10 16:00:33 +0300

    create SkillsController

--- HelloWorldWebApp/Data/ApplicationDbContext.cs
+++ HelloWorldWebApp/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WebApplication1.Models;
 
 namespace HelloWorldWebApp.Data
 {
@@ -12,5 +13,6 @@ namespace HelloWorldWebApp.Data
             : base(options)
         {
         }
+        public DbSet<WebApplication1.Models.Skill> Skill { get; set; }
     }
 }
